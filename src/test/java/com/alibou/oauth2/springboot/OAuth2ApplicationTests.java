9164db89dfd70d9c5cb27412b3a3318cc781e17f
Yacine Hammi <hammi.yacine.1@gmail.com>
2023-07-31 13:29:11 +0200

    first commit

---
+++ src/test/java/com/alibou/oauth2/springboot/OAuth2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alibou.oauth2.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OAuth2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
